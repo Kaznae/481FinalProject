7ec7596133aaa29bd05fa6b7b6a07430d42ee9f9
Mikael Yemane <40439037+mikaelyemane@users.noreply.github.com>
2019-04-23 13:54:43 -0400

    Add files via upload

---
+++ model2.py
@@ -0,0 +1,99 @@
+%matplotlib inline
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.api as sm 
+import statsmodels.formula.api as smf
+from statsmodels.graphics.gofplots import ProbPlot
+
+#import csv data
+data = pd.read_csv('data/data_100pp_all_variables_clean.csv')
+
+#define data as dataframe type 
+df = pd.DataFrame(data)
+#print(df)   
+
+# assign dependent and independent / explanatory variables
+y = '2014pm25'
+x = ['Solar_pct','Coal_pct','Gas_pct','Hydro_pct','Wind_pct','Waste_pct','Biomass_pct','Nuclear_pct','2014co2']
+
+# Ordinary least squares regression with constant term
+model = sm.OLS(df[y], sm.add_constant(df[x])).fit()
+print(model.summary())
+
+#print arrays of important values
+print(model.params)
+print("\n")
+print(model.pvalues)
+print("\n")
+print('R2: ', model.rsquared)
+
+plt.style.use('seaborn') # pretty matplotlib plots
+
+plt.rc('font', size=14)
+plt.rc('figure', titlesize=18)
+plt.rc('axes', labelsize=15)
+plt.rc('axes', titlesize=18)
+
+
+# fitted values (need a constant term for intercept)
+model_fitted_y = model.fittedvalues
+
+# model residuals
+model_residuals = model.resid
+
+# normalized residuals
+model_norm_residuals = model.get_influence().resid_studentized_internal
+
+# absolute residuals
+model_abs_resid = np.abs(model_residuals)
+
+# leverage, from statsmodels internals
+model_leverage = model.get_influence().hat_matrix_diag
+
+#Residual Plot
+plot_lm_1 = plt.figure(1)
+plot_lm_1.set_figheight(8)
+plot_lm_1.set_figwidth(12)
+
+plot_lm_1.axes[0] = sns.residplot(model_fitted_y, '2014pm25', data=data, 
+                          lowess=True, 
+                          scatter_kws={'alpha': 0.5}, 
+                          line_kws={'color': 'red', 'lw': 1, 'alpha': 0.8})
+
+plot_lm_1.axes[0].set_title('Residuals vs Fitted')
+plot_lm_1.axes[0].set_xlabel('Fitted values')
+plot_lm_1.axes[0].set_ylabel('Residuals')
+
+# annotations
+abs_resid = model_abs_resid.sort_values(ascending=False)
+abs_resid_top_3 = abs_resid[:3]
+
+for i in abs_resid_top_3.index:
+    plot_lm_1.axes[0].annotate(i, 
+                               xy=(model_fitted_y[i], 
+                                   model_residuals[i]));
+plt.savefig('figures/model2residuals.jpg')
+
+#QQ plot
+QQ = ProbPlot(model_norm_residuals)
+plot_lm_2 = QQ.qqplot(line='45', alpha=0.5, color='#4C72B0', lw=1)
+
+plot_lm_2.set_figheight(8)
+plot_lm_2.set_figwidth(12)
+
+plot_lm_2.axes[0].set_title('Normal Q-Q')
+plot_lm_2.axes[0].set_xlabel('Theoretical Quantiles')
+plot_lm_2.axes[0].set_ylabel('Standardized Residuals');
+
+# annotations
+abs_norm_resid = np.flip(np.argsort(np.abs(model_norm_residuals)), 0)
+abs_norm_resid_top_3 = abs_norm_resid[:3]
+
+for r, i in enumerate(abs_norm_resid_top_3):
+    plot_lm_2.axes[0].annotate(i, 
+                               xy=(np.flip(QQ.theoretical_quantiles, 0)[r],
+                                   model_norm_residuals[i]));
+plt.savefig('figures/model2QQplot.jpg')
